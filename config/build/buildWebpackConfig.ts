fb9a1735842a97440b45057e39d34f01d25946f1
yacenko <mikhail.yacenko@fabit.ru>
2023-02-15 00:39:18 +0300

    moved to yarn.lock, added webpack-dev-server

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,9 +3,10 @@ import {BuildOptions} from "./types/config";
 import {buildPlugins} from "./buildPlugins";
 import {buildRules} from "./buildRules";
 import {buildResolvers} from "./buildResolvers";
+import {buildDevServer} from "./buildDevServer";
 
 export const buildWebpackConfig = (options: BuildOptions): webpack.Configuration => {
-    const {mode, paths} = options;
+    const {mode, paths, isDev} = options;
 
     return {
         mode,
@@ -20,5 +21,7 @@ export const buildWebpackConfig = (options: BuildOptions): webpack.Configuration
             rules: buildRules(),
         },
         resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
     }
 }
